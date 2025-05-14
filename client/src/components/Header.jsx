14b6d9ab15e63997cb03e4e1f90a28abd5d4c2cb
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-05-14 17:43:26 +0530

    Header Component Completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,8 +13,8 @@ import {
 } from "react-icons/ri";
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false); // Controls main menu
-  const [reportsOpen, setReportsOpen] = useState(false); // Controls Tools dropdown
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [reportsOpen, setReportsOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
@@ -25,64 +25,197 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
-      <nav className="nav container">
-        <div className="nav__data">
-          <a href="/" className="nav__logo">
-            <img src={QuickFitLogo} alt="" />
-          </a>
+    <>
+      <div className="header-top">
+        <div>
+          <p>
+            ISO 9000 : 2015 Certified | ISO 14001 : 2015 Certified | OHSAS 18001
+            : 2015 Certified
+          </p>
+        </div>
+        <div className="header-top-countries">
+          {/* Most Known */}
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg"
+            alt="India"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg"
+            alt="United States"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg"
+            alt="France"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/9/9a/Flag_of_Spain.svg"
+            alt="Spain"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg"
+            alt="Netherlands"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/8/88/Flag_of_Australia_%28converted%29.svg"
+            alt="Australia"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg"
+            alt="Portugal"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg"
+            alt="Norway"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_United_Arab_Emirates.svg"
+            alt="United Arab Emirates"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/66/Flag_of_Malaysia.svg"
+            alt="Malaysia"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_Thailand.svg"
+            alt="Thailand"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Vietnam.svg"
+            alt="Vietnam"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Flag_of_Oman.svg"
+            alt="Oman"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/aa/Flag_of_Kuwait.svg"
+            alt="Kuwait"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Qatar.svg"
+            alt="Qatar"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/00/Flag_of_Palestine.svg"
+            alt="Palestine"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg"
+            alt="Brazil"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg"
+            alt="Mexico"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/79/Flag_of_Nigeria.svg"
+            alt="Nigeria"
+          />
 
-          <div className="nav__toggle" onClick={toggleMenu}>
-            {menuOpen ? (
-              <RiCloseLine className="nav__icon" />
-            ) : (
-              <RiMenuLine className="nav__icon" />
-            )}
+          {/* Less Known */}
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Angola.svg"
+            alt="Angola"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Flag_of_Mozambique.svg"
+            alt="Mozambique"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/00/Flag_of_Namibia.svg"
+            alt="Namibia"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_C%C3%B4te_d%27Ivoire.svg"
+            alt="Cote d'Ivoire"
+          />
+          <img
+            src="http://upload.wikimedia.org/wikipedia/commons/9/9c/Flag_of_Brunei.svg"
+            alt="Brunei"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_Guyana.svg"
+            alt="Guyana"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/60/Flag_of_Suriname.svg"
+            alt="Suriname"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/64/Flag_of_Trinidad_and_Tobago.svg"
+            alt="Trinidad and Tobago"
+          />
+        </div>
+      </div>
+      <header className="header">
+        <nav className="nav container">
+          <div className="nav__data">
+            <a href="/" className="nav__logo">
+              <img src={QuickFitLogo} alt="" />
+            </a>
+
+            <div className="nav__toggle" onClick={toggleMenu}>
+              {menuOpen ? (
+                <RiCloseLine className="nav__icon" />
+              ) : (
+                <RiMenuLine className="nav__icon" />
+              )}
+            </div>
           </div>
-        </div>
 
-        <div className={`nav__menu ${menuOpen ? "show-menu" : ""}`}>
-          <ul className="nav__list">
-            <li>
-              <Link to="/" className="nav__link">
-                HOME
-              </Link>
-            </li>
+          <div className={`nav__menu ${menuOpen ? "show-menu" : ""}`}>
+            <ul className="nav__list">
+              <li>
+                <Link to="/" className="nav__link">
+                  HOME
+                </Link>
+              </li>
 
-            <li>
-              <Link to="/about" className="nav__link">
-                ABOUT US
-              </Link>
-            </li>
+              <li>
+                <Link to="/about" className="nav__link">
+                  ABOUT US
+                </Link>
+              </li>
 
-            <li>
-              <Link to="/services" className="nav__link">
-                SERVICES
-              </Link>
-            </li>
-            <li>
-              <Link to="/team" className="nav__link">
-                OUR TEAM
-              </Link>
-            </li>
-            <li className="contact-hidden">
-              <Link to="/contact" className="nav__link">
-                CONTACT
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="nav__data">
-          <ul className="nav__hidden contact-btn">
-            <li>
-              <Link to="/contact" className="nav__link">
-                CONTACT
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+              <li>
+                <Link to="/products" className="nav__link">
+                  PRODUCTS
+                </Link>
+              </li>
+              <li>
+                <Link to="/industries" className="nav__link">
+                  INDUSTRIES
+                </Link>
+              </li>
+              <li>
+                <Link to="/blogs" className="nav__link">
+                  BLOGS
+                </Link>
+              </li>
+              <li>
+                <Link to="/gallery" className="nav__link">
+                  GALLERY
+                </Link>
+              </li>
+              <li className="contact-hidden">
+                <Link to="/contact" className="nav__link">
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="nav__data">
+            <ul className="nav__hidden contact-btn">
+              <li>
+                <Link to="/contact" className="nav__link">
+                  CONTACT US
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 };
 
